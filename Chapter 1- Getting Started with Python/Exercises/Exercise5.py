834086384f96d95a87e6a81571d36572edc86686
AeoniaRiawa <johntydusbalasa@gmail.com>
2023-10-04 11:28:15 +0400

    Exercise 5
    Exercise 5

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,12 @@
+from math import pi
+radius=float(input("Enter the radius : "))
+area = 3.14*radius*radius
+print("Area of the circle ", area)
+def area_circle(r):
+    area = 3.14 * r * r
+    print(area)
+r = float(input("Enter the radius : "))
+area_circle(r)  
+radius = float(input("Enter the radius :"))
+area = pi * ("radius ** 2")
+print(area)
